5c6a01ffa44a073d805ed7fa41bee010ba37b1d3
andy9 <ghostrider.anand@gmail.com>
2025-11-14 19:52:23 +0530

    Test

--- SeleniumProject/SeleniumTest/Main/ADBConnect.py
+++ SeleniumProject/SeleniumTest/Main/ADBConnect.py
@@ -1,11 +1,21 @@
 from ppadb.client import Client as AdbClient
 
-def run_adb_command_with_ppadb():
+def run_adb_command_with_ppadb(udid):
+    udid = udid
+    try:
+        if udid != "":
+            print(f"ADB workflow for : {udid}")
+        else:
+            print("No UDID to proceed")
+            return
+    except Exception as e:
+        print(f"No UDID: {e}")
+
     # Connect to the ADB server
     client = AdbClient(host="127.0.0.1", port=5037)
 
     #Connect to a device
-    client.remote_connect("192.168.50.24", 5555)
+    client.remote_connect(udid, 5555)
 
     # Get a list of connected devices
     devices = client.devices()
@@ -26,7 +36,7 @@ def run_adb_command_with_ppadb():
         print(f"Error executing command: {e}")
 
     #Disconnect device
-    client.remote_disconnect("192.168.50.24")
+    client.remote_disconnect(udid)
 
 if __name__ == "__main__":
-    run_adb_command_with_ppadb()
+    run_adb_command_with_ppadb("192.168.50.24")
